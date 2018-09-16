bfdbd6f58036bdb85d1a6233a5a1b3eaec00cf75
Jeremy Philipson <jeremyphilipson@gmail.com>
2018-09-15 20:35:18 -0400

    minor styling

--- src/Entry.js
+++ src/Entry.js
@@ -5,7 +5,7 @@ const Entry = ({ entry, teamWinMap, makeSentenceCase, scoreSorted, rank }) => {
   const teams = scoreSorted ? null : entry.selections
   const entryScore = scoreSorted ? null : teams.reduce((memo, team) => memo += teamWinMap[team], 0)
   return (
-    <div key={entry.id} style={{ display: 'grid', gridTemplateColumns: '75% 20%', backgroundColor: `${rank % 2 ? '#d8d8d8' : '#eee' }` }}>
+    <div key={entry.id} style={{ display: 'grid', gridTemplateColumns: '75% 20%', backgroundColor: `${rank % 2 ? '#d8d8d8' : '#eee' }`, padding: '5px 5px 0px 10px'}}>
       <div>
         <Link to={`/entry/${entry.id}`}>
           <h4>{/* scoreSorted ? `${rank+1})` : null */} {makeSentenceCase(entry.teamName)}</h4>
