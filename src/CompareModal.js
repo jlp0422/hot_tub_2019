5a46e11cc74df248f64eebd0fe1421bd05d13f0b
Jeremy Philipson <jeremyphilipson@gmail.com>
2018-10-02 11:56:30 -0400

    some more minor breakpoint stuff

--- src/CompareModal.js
+++ src/CompareModal.js
@@ -4,26 +4,33 @@ import MediaQuery from 'react-responsive';
 import { makeSentenceCase } from './utils';
 
 const CompareModal = ({ showModal, closeModal, compareTeams, entries, teamCityName, teamWinMap }) => {
-  const selectedTeams = compareTeams.length
+  const numberOfTeams = compareTeams.length
   const teams = entries.reduce((memo, entry) => {
     compareTeams.includes(entry.id) && memo.push(entry)
     return memo
   }, [])
   return (
-    <ReactModal isOpen={showModal} shouldCloseOnEsc={true} shouldCloseOnOverlayClick={true} onRequestClose={closeModal} style={{
-      content: {
-      }
-    }}>
-      <h2>Compare Teams</h2>
-      <div className="grid grid-compare margin-b-15" style={{ gridTemplateColumns: 'auto '.repeat(selectedTeams)}}>
+    <ReactModal isOpen={showModal} shouldCloseOnEsc={true} shouldCloseOnOverlayClick={true} onRequestClose={closeModal}>
+      <h2>Comparing {numberOfTeams} Teams</h2>
+      <div className="grid grid-compare margin-b-15" style={{ gridTemplateColumns: 'auto '.repeat(numberOfTeams)}}>
       {
         teams.map((team, index) => (
           <div style={{ /*gridRowStart: index+1 */}} key={team.id}>
             <h5>Team Name: { makeSentenceCase(team.teamName)}</h5>
             <h6>Wins: {team.selections.reduce((memo, team) => memo += teamWinMap[team], 0)}</h6>
             <ul>
-              { team.selections.sort().map(selection => (
-                  <li key={selection}>{teamCityName[selection]}&nbsp;&nbsp;<span className="badge badge-secondary badge-pill">{`${teamWinMap[selection]} ${teamWinMap[selection] === 1 ? 'win' : 'wins'}`}</span></li>
+                {team.selections.sort().map(selection => (
+                  <li key={selection}>
+                    <MediaQuery query="(min-width: 661px)">
+                      {teamCityName[selection]}&nbsp;&nbsp;
+                    </MediaQuery>
+                    <MediaQuery query="(max-width: 660px)">
+                      {selection}&nbsp;&nbsp;
+                    </MediaQuery>
+                    <span className="badge badge-secondary badge-pill">
+                      {`${teamWinMap[selection]} ${teamWinMap[selection] === 1 ? 'win' : 'wins'}`}
+                    </span>
+                  </li>
               ))}
             </ul>
           </div>
