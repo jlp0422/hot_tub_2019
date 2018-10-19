cee07a1d3395ee467c1a92239fffc9eed18c0d51
Jeremy Philipson <jeremyphilipson@gmail.com>
2018-10-18 23:43:37 -0400

    less network requests

--- src/TeamEntry.js
+++ src/TeamEntry.js
@@ -41,16 +41,18 @@ const _TeamEntry = ({
   const entry = entries.find(entry => entry.id === id * 1)
   const totalScore = entry.selections.reduce((memo, team) => memo += teamWinMap[team], 0)
   const apiTeams = entry.selections.join(',')
-  axios.get(`/api/games/seasonal/regular/2018/${apiTeams}`)
-    .then(resp => resp.data[0].games)
-    .then(allGames => allGamesToWeeksObject(allGames))
-    .then(gamesPerWeek => totalWinsForWeek(gamesPerWeek, entry.selections))
-    .then(weeklyWins => changeState(weeklyWins))
-    .catch(err => console.error(err))
-
+  if (!Object.keys(winsPerWeekObject).length) {
+    axios.get(`/api/games/seasonal/regular/2018/${apiTeams}`)
+      .then(resp => resp.data[0].games)
+      .then(allGames => allGamesToWeeksObject(allGames))
+      .then(gamesPerWeek => totalWinsForWeek(gamesPerWeek, entry.selections))
+      .then(weeklyWins => changeState(weeklyWins))
+      .catch(err => {
+        throw new Error('Network error. Please try again')
+      })
+  }
   if (!tab) tab = 'teams'
   if (!entry.id) return null;
-  console.log(winsPerWeekObject)
   return (
     <div>
       <h2>Team Name: {makeSentenceCase(entry.teamName)}</h2>
@@ -83,9 +85,9 @@ const _TeamEntry = ({
           <div>
             {
               Object.keys(winsPerWeekObject).map(weekName => (
-                <div className="grid" style={{ gridTemplateColumns: '70% 25%', gridColumnGap: '5%' }}>
-                    <h4>{weekName}</h4>
-                    <h4>{winsPerWeekObject[weekName]}</h4>
+                <div key={weekName} className="grid" style={{ gridTemplateColumns: '70% 25%', gridColumnGap: '5%' }}>
+                  <h4>{weekName}</h4>
+                  <h4>{winsPerWeekObject[weekName]}</h4>
                 </div>
               ))
             }
