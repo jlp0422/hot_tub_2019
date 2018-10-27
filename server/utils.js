f7b4e7636bdbe7aae26877cf56966d57365609bd
Jeremy Philipson <jeremyphilipson@gmail.com>
2018-10-27 18:21:07 -0400

    hide api

--- server/utils.js
+++ server/utils.js
@@ -1,5 +1,4 @@
 const asyncMiddleware = (fn) => (req, res, next) => {
-  console.log('middleware')
   Promise
     .resolve(fn(req, res, next))
     .catch(next)
@@ -7,7 +6,7 @@ const asyncMiddleware = (fn) => (req, res, next) => {
 
 const MySportsFeeds = require('mysportsfeeds-node');
 const msfTwo = new MySportsFeeds('2.0', true);
-msfTwo.authenticate('b57b9597-7a4b-4b7b-b6b7-10fd58', 'MYSPORTSFEEDS');
+msfTwo.authenticate(process.env.MSF_API, 'MYSPORTSFEEDS');
 
 module.exports = {
   asyncMiddleware,
