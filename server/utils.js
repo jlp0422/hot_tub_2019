9b683881a26966869b013317cbb609d40938acc7
Jeremy Philipson <jeremyphilipson@gmail.com>
2018-12-05 19:50:00 -0500

    attempting heroku push

--- server/utils.js
+++ server/utils.js
@@ -1,14 +1,14 @@
-const asyncMiddleware = (fn) => (req, res, next) => {
+const asyncMiddleware = (fn, cb) => (req, res, next) => {
   Promise
     .resolve(fn(req, res, next))
     .catch(next)
-}
+};
 
-const MySportsFeeds = require('mysportsfeeds-node');
-const msfTwo = new MySportsFeeds('2.0', true);
-msfTwo.authenticate(process.env.MSF_API, 'MYSPORTSFEEDS');
+const MySportsFeeds = require("mysportsfeeds-node");
+const msfTwo = new MySportsFeeds("2.0", true);
+msfTwo.authenticate(process.env.MSF_API, "MYSPORTSFEEDS");
 
 module.exports = {
   asyncMiddleware,
   msfTwo
-}
+};
