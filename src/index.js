30397cc1a37ead36641bac2ae0b3f250c63117a9
Jeremy Philipson <jeremyphilipson@gmail.com>
2019-09-03 22:50:59 -0400

    chore: update google analytics

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactModal from 'react-modal'
 import App from './App'
 import ReactGA from 'react-ga'
 
-ReactGA.initialize('UA-128159832-1')
+ReactGA.initialize('UA-128159832-2')
 ReactModal.setAppElement('#root')
 
 const root = document.getElementById('root')
